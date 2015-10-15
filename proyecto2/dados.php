1268658d15c8d37f27613b82e47741dced09b6a4
Francisco Perez <frankiko.8@gmail.com>
2015-10-15 13:08:56 +0000

    proyecto2

---
+++ proyecto2/dados.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+	$min=1;
+	$max=6;
+	$result = rand($min , $max);
+	
+
+?>
+ <img src="imagen/caca<?php echo $result ?>.jpg"/>
+ 
+</body>
+</html>
---
+++ proyecto2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        
+        <?php
+        
+            include 'configuracion.php';
+            include 'dados.php'
+        
+        ?>
+        
+    </body>
+</html>
