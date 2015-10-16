930b8f3ed3a466ba902d3f7f4a53c65bf4f9b073
Francisco Perez <frankiko.8@gmail.com>
2015-10-16 13:32:58 +0000

    proyectos actualizados

--- proyecto2/dado.php
+++ proyecto2/dado.php
@@ -14,7 +14,7 @@
 	<div class='row'>
 		
 		<div class='col-md-2'><h3>Dado Seis Caras: 6</h3><img src="imagen/dado<?php echo dados(1,6)?>.png"/></div>
-	
+		<div class='col-md-2'><h3>Dado Tres Caras: 3</h3><img src="imagen/dado<?php echo dados(1,3)?>.png"/></div>
 		
 </div>
 	
